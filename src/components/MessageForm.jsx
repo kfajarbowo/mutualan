4adb439b5f1eee4b42588ffb5b2a6fc7eefea711
kfajarbowo <bagas@workshop.web.id>
2021-03-15 18:11:53 +0700

    first commit

---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,12 @@
+const MessageForm = () =>{
+    const handleSubmit = () =>{
+
+    }
+    return(
+        <form className="message-form" onSubmit={handleSubmit} action="">
+            <input type="text" name="" id=""/>
+        </form>
+    )
+}
+
+export default MessageForm;
