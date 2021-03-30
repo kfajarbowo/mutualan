dbd1932950ddb9fc094aff7fe04f57e803761ec6
kfajarbowo <bagas@workshop.web.id>
2021-03-30 12:40:11 +0700

    update auth from user

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 const App = () =>{
+	if(!localStorage.getItem('username')) return <LoginForm/>
     return (
 			<ChatEngine
 				height="100vh"
-				userName="kidung"
-				userSecret="fajar"
+				userName={localStorage.getItem('username')}
+				userSecret={localStorage.getItem('password')}
 				projectID="2d49e31a-7b30-4390-99ef-f7e7ab996d31"
 				renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 			/>
